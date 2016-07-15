66b11460a3c7ddc4b45eff2c8b2f5f48c3845522
Arjo Kalisvaart <amplifiedart@gmail.com>
2016-07-15 10:04:16 +0200

    Initial Contracts established needs additonal work.

---
+++ OD.Data/OD.Data/IDataConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OD.Data
+{
+	/// <summary>
+	/// Contract for a connection to a Data Provider
+	/// </summary>
+	public interface IDataConnection
+	{
+		/// <summary>
+		/// Opens the continuous connection.
+		/// </summary>
+		void OpenContinuousConnection();
+
+		/// <summary>
+		/// Closes the continuous connection.
+		/// </summary>
+		void CloseContinuousConnection();
+
+		/// <summary>
+		/// Gets a value indicating whether this instance is a continuous connection.
+		/// </summary>
+		/// <value>
+		/// <c>true</c> if this instance is continuous connection; otherwise, <c>false</c>.
+		/// </value>
+		bool IsContinuousConnection { get; }
+	}
+}
---
+++ OD.Data/OD.Data/IDataProcedure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OD.Data
+{
+	public interface IDataProcedure
+	{
+		string Procedure { get; } 
+
+		
+	}
+}
